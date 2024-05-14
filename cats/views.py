4a1439df381104872739f64bd89f4b405d88203b
ISNekrasova <133829625+ISNekrasova@users.noreply.github.com>
2024-05-14 05:04:22 +0300

    ))

--- cats/views.py
+++ cats/views.py
@@ -2,34 +2,39 @@
 # from rest_framework.decorators import api_view
 # from rest_framework.response import Response
 # from rest_framework.views import APIView
+# from rest_framework import generics
 
-from rest_framework import generics
+from rest_framework import viewsets 
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
-class CatList(generics.ListCreateAPIView):
+class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
-    serializer_class = CatSerializer
+    serializer_class = CatSerializer 
+
+# class CatList(generics.ListCreateAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
 
 
-class CatDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Cat.objects.all()
-    serializer_class = CatSerializer
+# class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
 
 # class APICat(APIView):
-#     def get(self, request):
-#         cats = Cat.objects.all()
-#         serializer = CatSerializer(cats, many=True)
-#         return Response(serializer.data)
+    # def get(self, request):
+    #     cats = Cat.objects.all()
+    #     serializer = CatSerializer(cats, many=True)
+    #     return Response(serializer.data)
 
-#     def post(self, request):
-#         serializer = CatSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED) 
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+    # def post(self, request):
+    #     serializer = CatSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED) 
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
 
 # @api_view(['GET', 'POST'])
 # def cat_list(request):
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,11 +1,19 @@
-from django.urls import path
+from rest_framework.routers import SimpleRouter
 
-from cats.views import CatList, CatDetail
+from django.urls import include, path
 
+from cats.views import CatViewSet
+
+router = SimpleRouter()
+router.register('cats', CatViewSet)
 urlpatterns = [
-    path('cats/', CatList.as_view()),
-    path('cats/<int:pk>/', CatDetail.as_view()),
-]
+    path('', include(router.urls)),
+] 
+
+# urlpatterns = [
+#     path('cats/', CatList.as_view()),
+#     path('cats/<int:pk>/', CatDetail.as_view()),
+# ]
 
 # urlpatterns = [
 #     path('cats/', APICat.as_view()),
